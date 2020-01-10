81d5dc9f2985b8c56a8fd9c6bc7e594419a7be2f
Courtney Gerver <courtneygerver@Courtneys-MacBook-Pro.local>
2020-01-10 13:45:50 -0500

    Finalized files for #tidytuesday week2 - Australian Fires

--- week02_AustralianFires/australianFiresPlot.R
+++ week02_AustralianFires/australianFiresPlot.R
@@ -61,13 +61,47 @@ theme_set(theme_bw())
 averages[averages == ""] <- NA
 averages[rowSums(is.na(averages)) != ncol(averages), ] 
 
+averages %>%                      
+   group_by(city_name) %>%                                 
+   summarise(minYear = min(year, na.rm=T))
+averages<-subset(averages, averages[ , 1] > 1964)
+
+##Average temperature
 p <- ggplot(averages,
-   aes(year, averageTemperature, group = city_name, color = factor(city_name))) +
+   aes(as.numeric(year), averageTemperature, group = city_name, color = factor(city_name))) +
    geom_line() +
-   scale_color_viridis_d() +
-   labs(x = "year", y = "Temperature") +
-   theme(legend.position = "top")
+   #scale_color_viridis_d() +
+   labs(x = "Year", y = "Temperature (°C)") +
+   ggtitle("Temperature in Australian Cities Over Time") +
+   theme(plot.title=element_text(hjust = .5, color = "black",size=16,face="bold")) +
+   scale_color_manual(values = c("salmon", "darkgoldenrod1","plum3","deepskyblue","darkseagreen1"), 
+                      labels=c("Brisbane","Canberra","Melbourne","Perth","Sydney"), name = "City Name")+
+   scale_x_continuous(breaks = c(1965,1975,1985,1995,2005,2015)) 
 p
 
-f<-p + transition_reveal(year)
-animate(f, nframes = 100, renderer = gifski_renderer("gganim.gif"))
+f<-p +  
+   geom_point() +
+   transition_reveal(year)
+
+#Save out gif
+animate(f, nframes = 55, renderer = gifski_renderer("aussieTempsOverTime.gif"))
+
+##Average rainfall
+q <- ggplot(averages,
+            aes(as.numeric(year), averageRainfall, group = city_name, color = factor(city_name))) +
+   geom_line() +
+   #scale_color_viridis_d() +
+   labs(x = "Year", y = "Rainfall (inches)") +
+   ggtitle("Rainfall in Australian Cities Over Time") +
+   theme(plot.title=element_text(hjust = .5, color = "black",size=16,face="bold")) +
+   scale_color_manual(values = c("salmon", "darkgoldenrod1","plum3","deepskyblue","darkseagreen1"), 
+                      labels=c("Brisbane","Canberra","Melbourne","Perth","Sydney"), name = "City Name")+
+   scale_x_continuous(breaks = c(1965,1975,1985,1995,2005,2015)) 
+q
+
+g<-q +  
+   geom_point() +
+   transition_reveal(year)
+
+#Save out gif
+animate(g, nframes = 55, renderer = gifski_renderer("aussieRainfallOverTime.gif"))
