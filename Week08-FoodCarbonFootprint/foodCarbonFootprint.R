7712634834385ee3438a111b6d8e70c4324b5078
Courtney Gerver <courtneygerver@gmail.com>
2020-02-22 00:24:27 -0500

    CO2 emissions for the beef industry worldwide

---
+++ Week08-FoodCarbonFootprint/foodCarbonFootprint.R
@@ -0,0 +1,46 @@
+
+# Read in data --------------------------------------------------------------------------------
+
+food_consumption <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-18/food_consumption.csv')
+
+# Prep for plot -------------------------------------------------------------------------------
+
+#libraries
+library(tidyverse)
+library(rnaturalearth)
+
+#What industry emits the most CO2?
+food_consumption %>%
+   group_by(food_category) %>%
+   summarise(mean = mean(co2_emmission), n = n()) 
+##Beef, no surprise there
+
+#Get world variable
+world <- ne_countries(scale = "medium", returnclass = "sf") 
+
+#Extract beef for plot
+beef <- food_consumption %>%
+   filter(food_category == "Beef") %>%
+   mutate(country.corr = str_replace(country,"USA","United States"))
+worldBeef <- left_join(world,beef,by=c('name'='country.corr'))
+
+#Remove Antartica
+worldBeef<-filter(worldBeef,geounit != "Antarctica") 
+
+# Plot ----------------------------------------------------------------------------------------
+
+beefPlot <- ggplot(data = worldBeef) +
+   geom_sf(aes(fill=co2_emmission)) +
+   scale_fill_viridis_c("Emissions \n (Kg CO2/person/yr)", direction = -1) +
+   theme_minimal() +
+   theme(plot.title=element_text(hjust = .5, color = "black",face="bold",size=18),
+         axis.text.x=element_text(color = "black",face="bold",size=14),
+         axis.title.x=element_text(hjust = .5, color = "black",face="bold",size=16),
+         axis.text.y=element_text(color = "black",face="bold",size=14),
+         axis.title.y=element_text(hjust = .5, color = "black",face="bold",size=16)) + 
+   labs(title = (''~CO[2]~' emissions from beef consumption')) 
+
+#Save out plot
+png('beefPlot.png', width=7, height=6, units = 'in',res=150)
+beefPlot 
+dev.off()
